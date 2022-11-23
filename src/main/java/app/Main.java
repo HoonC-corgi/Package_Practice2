65f672124d57f6c57460a68934b7c4c52e30aab7
HoonC-corgi <tgh7544@naver.com>
2022-11-23 16:39:01 +0900

    Package practice 2

---
+++ src/main/java/app/Main.java
@@ -0,0 +1,12 @@
+package app;
+
+import base.Shape;
+import derived.Circle;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Shape s = new Circle();
+        s.draw();
+    }
+}
